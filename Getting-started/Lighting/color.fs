0e708760dba3ce3f22620e939baf56c0320eccce
岩 曹 <935524537@qq.com>
2020-12-24 16:45:21 +0800

    略微完善冯氏光照模型，添加部分注释

--- Getting-started/Lighting/color.fs
+++ Getting-started/Lighting/color.fs
@@ -11,21 +11,21 @@ uniform vec3 objectColor;
 
 void main()
 {
-
+    //环境光
     float ambientStrength = 0.1;
     vec3 ambient = ambientStrength * lightColor;
-
+    //漫反射
     vec3 norm = normalize(Normal);
     vec3 lightDir = normalize(lightPos - FragPos);
     float diff = max(dot(norm, lightDir), 0.0);
     vec3 diffuse = diff * lightColor;
-    
+    //镜面反射
     float specularStrength = 0.5;
     vec3 viewDir = normalize(viewPos - FragPos);
     vec3 reflectDir = reflect(-lightDir, norm);
     float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
     vec3 specular = specularStrength * spec * lightColor;
-
+    //相加
     vec3 result = (ambient + diffuse +specular) * objectColor;
     FragColor = vec4(result, 1.0);
 }
